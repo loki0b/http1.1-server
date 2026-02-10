55a8e05d1d5f22cd57b86f2a91ac89b271f6a5b1
loki0b <contato@henriquegarcialeite.com>
2026-02-10 11:13:18 -0300

    add: request line parser and aux functions

--- src/parser.c
+++ src/parser.c
@@ -1,8 +1,16 @@
+#include <stdlib.h>
+
 #define DEL 0x7F
 #define SP  0x20
 #define HT  0x09
 #define CR  0x0D
 #define LF  0x0A
+
+struct request_line {
+    char* method;
+    char* URI;
+    char* version;
+};
 
 int is_CTL(char byte) {
     if ((byte - 31 <= 0) || (byte == DEL)) return 1;
@@ -57,3 +65,45 @@ int LWS(char* byte, int size, char** next_char_pointer) {
     if (i > 0) return 1; //LWS
     else return 0;
 }
+
+// Search for a CRLF.
+// Return index of LF or -1 if not match
+int CRLF(char* buffer, int buffer_size) {
+    int i = 0;
+    while (i < (buffer_size - 1)) {
+        if (buffer[i] == CR && buffer[i + 1] == LF) {
+            i++;
+            return i;
+        }
+        i++;
+    }
+
+    return -1;
+}
+
+int separate_token_request_line(char* buffer, int buffer_size, char** element) {
+    int i = 0;
+    while (i < buffer_size && buffer[i] != SP) i++;
+    char* token = (char*) malloc((sizeof(char) * i) + 1);
+    //if (token == NULL) return NULL;
+
+    for (int j = 0; j < i + 1; j++) token[j] = buffer[j];
+    token[i + 1] = '\0';
+    *element = token;
+
+    return i + 1;
+}
+
+struct request_line* parse_request_line(char* buffer, int buffer_size) {
+    int reqli = CRLF(buffer, buffer_size);
+    if (reqli == -1); // Do something
+    
+    struct request_line* rl = (struct request_line*) malloc(sizeof(struct request_line));
+    if (rl == NULL) return NULL;
+
+    int ret = separate_token_request_line(buffer, buffer_size, &rl->method);
+    ret = separate_token_request_line(&buffer[ret], buffer_size - ret, &rl->URI);
+    ret = separate_token_request_line(&buffer[ret], buffer_size - ret, &rl->version);
+
+    return rl;
+} 
